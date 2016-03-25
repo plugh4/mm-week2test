4457f475f9b73876b8e77abc1480a61ff7f56174
Chris Serra <csserra@gmail.com@gmail.com>
2016-03-25 09:34:01 -0700

    Initial Commit

---
+++ mm-week2test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mm-week2test
+//
+//  Created by Christopher Serra on 3/25/16.
+//  Copyright © 2016 plugh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mm-week2test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mm-week2test
+//
+//  Created by Christopher Serra on 3/25/16.
+//  Copyright © 2016 plugh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
